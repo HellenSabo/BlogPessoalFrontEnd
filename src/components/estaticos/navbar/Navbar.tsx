531e3c4e533fa8fc3bb235f6ea7585813ee6aa06
Hellen Sabo <hellen.geekmais@gmail.com>
2022-09-08 15:56:29 -0300

    Configurando a tela de login durante a junção com o backend

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,43 +1,50 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography} from '@material-ui/core';
 import { Box } from '@mui/material';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+import { color } from '@mui/system';
+
 function Navbar() {
     return (
         <>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer" }} >
+                    <Box className = 'cursor' >
                         <Typography variant="h5" color="inherit">
-                            BlogPessoal
+                            Blog Pessoal
                         </Typography>
                     </Box>
-
+                    
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                home
+                                Home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                postagens
+                                Postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                temas
+                                Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                cadastrar tema
+                                Cadastrar tema
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Link to = '/login' className = 'text-decorator-none' >
+                         <Box mx={1} className = 'cursor'>
                             <Typography variant="h6" color="inherit">
-                                logout
+                                Logout
                             </Typography>
                         </Box>
+                         </Link>
+                       
                     </Box>
 
                 </Toolbar>
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,43 +1,77 @@
-import { Grid, TextField, Typography, Button } from '@material-ui/core';
-import { Box } from '@mui/material';
-import { Link } from 'react-router-dom';
-import './Login.css'
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Grid,Typography, TextField, Button } from '@material-ui/core';
+import { Box } from '@mui/material'
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { api } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
+import './Login.css';
+
 
 function Login() {
-    return (
-      <Grid container direction='row' justifyContent='center' alignItems='center'>
-        <Grid alignItems='center' xs={6}>
-          <Box paddingX={20}>
-            <form>
-              <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='color-weight'>Entrar</Typography>
-              <TextField id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-              <TextField id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
-              <Box marginTop={2} textAlign='center'>
-                <Link to='/home' className='text-decorator-none'>
-                  <Button type='submit' variant='contained' color='primary' className='btn'>Logar</Button>
-                </Link>
-              </Box>
-            </form>
-            <Box display='flex' justifyContent='center' marginTop={2}>
-              <Box marginRight={1}>
-                <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
-              </Box>
-              <Typography variant='subtitle1' gutterBottom align='center' className='color-weight'>Cadastre-se</Typography>
+  let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+            useEffect(()=>{
+                if(token != ''){
+                    navigate('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                const resposta = await api.post(`/usuarios/logar`, userLogin)
+                setToken(resposta.data.token)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
+        }
+  return (
+    <Grid container direction='row' justifyContent='center' alignItems='center'>
+      <Grid alignItems='center' xs={6}>
+        <Box paddingX={20}>
+          <form>
+            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos'>Entrar</Typography>
+            <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+            <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+            <Box marginTop={2} textAlign='center'>
+                <Button type='submit' variant='contained' color='primary' className='btn'>Logar</Button>
             </Box>
+          </form>
+          <Box display='flex' justifyContent='center' marginTop={2}>
+            <Box marginRight={1}>
+              <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+            </Box>
+            <Link to = '/cadastrousuario'>
+            <Typography variant='subtitle1' gutterBottom align='center'className='textos'>Cadastre-se</Typography>
+            </Link>
           </Box>
-        </Grid>
-        <Grid xs={6} className='imagem' style={{
-         backgroundImage: `url(https://i.imgur.com/2mtAelJ.jpeg)`,
-          backgroundRepeat: 'no-repeat',
-          width: '100vh',
-          minHeight: '100vh',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}>
-
-        </Grid>
+        </Box>
       </Grid>
-    );
-  }
+      <Grid xs={6} className='imagem'>
 
-  export default Login;
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Login;
